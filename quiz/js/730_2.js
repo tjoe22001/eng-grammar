fe325f9cfc619ac3200d5adbf68ebf59409f23ad
tjoe22001 <107105433+tjoe22001@users.noreply.github.com>
2022-06-26 12:32:09 +0900

    Add files via upload

---
+++ quiz/js/730_2.js
@@ -0,0 +1,105 @@
+	//問題文を格納する要素です
+	const quiz = [
+        {
+          question: "A large _____ of fish and sea mammals inhabit the coral reef off the western coast of the island.",
+          answers: ["vary", "varying", "variety", "variation"],
+          correct: "variety"
+        },
+        {
+          question: "_____ wishing to be considered for the opening should submit an application to the personnel department.",
+          answers: ["They", "Who", "That", "Those"],
+          correct: "just"
+        },
+        {
+          question: "Because of the Internet, information can be accessed _____ than at any time in the past.",
+          answers: ["easier", "more easily" ,"easiest", "most easily"],
+          correct: "more easily"
+        },
+        {
+          question: "The internship offers an opportunity for gaining valuable experience _____ those wishing to enter the film industry.",
+          answers: ["at", "of", "to", "by"],
+          correct: "to"
+        },
+        {
+          question: "Our company is commited to _____ natural resources via recycling programs and increased efficiency.",
+          answers: ["conserving", "conserve", "conservation", "conserves"],
+          correct: "conserving"
+        },
+        {
+          question: "The president suggested that the company Web site be updated _____ two weeks.",
+          answers: ["less", "every", "most", "much"],
+          correct: "every"
+        },
+        {
+          question: "We would prefer that the new office equipment _____ outside of our usual business hours.",
+          answers: ["be installed", "to install", "has installed", "will install"],
+          correct: "be installed"
+        },
+        {
+          question: "_____ employees eat in the break room, they should make sure to properly dispose of any garbage.",
+          answers: ["Always", "Following", "Nowadays", "Whenever"],
+          correct: "Whenever"
+        },
+        {
+          question: "There are too many candidates to speak with here today, so several interviews will be _____ at company headquarters next week.",
+          answers: ["notified", "nominated", "conducted", "reminded"],
+          correct: "conducted"
+        },
+        {
+          question: "A sales _____ unexpectedly dropped by headquarters, asking to meet with the head of the purchasing department.",
+          answers: ["represent", "representative", "representing", "representation"],
+          correct: "representative"
+        },
+        {
+          question: "_____ we expand internationally or continue operating only domestically depends on the results of the upcoming cross-border trade negotiations.",
+          answers: ["Whichever", "Neither", "Whether", "Even"],
+          correct: "Whether"
+        },
+        {
+          question: "As he has saved a substantial amount of money, Mr.Leon will be establishing _____ company next month.",
+          answers: ["on his", "he", "himself", "his own"],
+          correct: "his own"
+        },
+        {
+          question: "Although a sudden, heavy rainfall is helping firefighters control the blaze, it almost certainly will _____ the investigation into what caused the fire.",
+          answers: ["wait", "delay", "remain", "occur"],
+          correct: "delay"
+        },
+        {
+          question: "The board asked Mr.Han to provide them with more detailed information and a _____ explanation of the proposal than he gave them in his original presentation.",
+          answers: ["clear", "clearer", "clearly", "clearness"],
+          correct: "clearer"
+        },
+        {
+          question: "_____ of the packages show evidence of damage due to improper handling.",
+          answers: ["Several", "Whichever", "Something", "Every"],
+          correct: "Several"
+        },
+        {
+          question: "A request _____ classified data files must be accompanied by a supervisor's written authorization.",
+          answers: ["access", "to access", "access", "accsessible"],
+          correct: "to access"
+        },
+        {
+          question: "The first essay submitted by the student had numerous grammatical errors, _____ the second was nearly flawless.",
+          answers: ["since", "while", "if", "after"],
+          correct: "while"
+        },
+        {
+          question: "The weather service predicts that it _____ more heavily than usual.",
+          answers: ["has been raining", "is raining", "has rained", "will rain"],
+          correct: "will rain"
+        },
+        {
+          question: "A document _____ to this e-mail contains some important statistics obtained in the recent marketing study.",
+          answers: ["combined", "assciated", "inserted", "attched"],
+          correct: "attched"
+        },
+        {
+          question: "Eagle Mart, one of the biggest shoe stores in town, stays _____ untill midnight on Fridays and Saturdays.",
+          answers: ["open", "opening", "openly", "to open"],
+          correct: "open"
+        }
+      ];
+    
+  
---
+++ quiz/js/pinpoint.js
@@ -0,0 +1,43 @@
+//問題文を格納する要素です
+const quiz = [
+    {
+      question: "_____ of the employees planning to attend the seminar have to apply for it no later than April 10.",
+      answers: ["Each", "Every", "All", "Another"],
+      correct: "All"
+    },
+    {
+      question: "According to yesterday's news report, the Asian economy is going to _____ soon.",
+      answers: ["replace", "recover", "reduce", "receive"],
+      correct: "recover"
+    },
+    {
+      question: "Several of the section managers urged their workers to _____ participating in this week's training session.",
+      answers: ["demand", "consider" ,"complain", "request"],
+      correct: "consider"
+    },
+    {
+      question: "Please become a member of the Keystone Art Gallery by _____ for a membership on our Web site.",
+      answers: ["registering", "attending", "assigning", "relying"],
+      correct: "registering"
+    },
+    {
+      question: "As of February 1, regular attendance and active _____ at the weekly staff meeting will be required of all aupervisors.",
+      answers: ["absence", "participation", "acquisition", "joint"],
+      correct: "participation"
+    },
+    {
+      question: "Although their schedule is tight, the florists are doing everything they can to meet the _____.",
+      answers: ["intention", "tenure", "deadline", "capacity"],
+      correct: "deadline"
+    },
+    {
+      question: "The new sales team has performed far _____ the expectations of management.",
+      answers: ["out of", "owing to", "without", "beyond"],
+      correct: "beyond"
+    },
+    {
+      question: "According to Maztap Limited's financial report, the company's online sales have _____ increased over the past six years.",
+      answers: ["enthusiastically", "consistently", "commonly", "discreetly"],
+      correct: "consistently"
+    }
+  ];
