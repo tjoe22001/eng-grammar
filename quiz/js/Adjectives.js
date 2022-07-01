2832ac88bd741b54aab4e915f8442fdb40ed3c76
tjoe22001 <107105433+tjoe22001@users.noreply.github.com>
2022-07-01 21:15:35 +0900

    Add files via upload

---
+++ quiz/js/Adjectives.js
@@ -0,0 +1,89 @@
+//問題文を格納する要素です
+const quiz = [
+    {
+      question: "The new marketing manager, Glenn Park, is _____ of coordinating many projects simultaneously.",
+      answers: ["able", "praised", "capable", "proficient"],
+      correct: "capable"
+    },
+    {
+      question: "Ms.Rogers is proud of being an accountant at Lipton LLP, one of the most _____ law firms in San Diego.",
+      answers: ["numerous", "tremendous", "anxious", "prestigious"],
+      correct: "prestigious"
+    },
+    {
+      question: "On behalf Alston & Prichard LLP, we are _____ to inform you that your patent has been approved.",
+      answers: ["prompted", "pleased" ,"verified", "impressed"],
+      correct: "pleased"
+    },
+    {
+      question: "The instruction manual of the 3DX is easy to read and very _____ even for beginners.",
+      answers: ["respectful", "helpful", "successful", "doubtful"],
+      correct: "helpful"
+    },
+    {
+      question: "The audio-visual equipment in the fifth-floor supply room is _____ exclusively to marketing department personnel.",
+      answers: ["affordable", "available", "probable", "dependable"],
+      correct: "available"
+    },
+    {
+      question: "All of the patients in this hospital are properly instructed about the _____ amount of medicine to take.",
+      answers: ["appropriate", "immediate", "spacious", "delicious"],
+      correct: "appropriate"
+    },
+    {
+      question: "Some common software programs are not currently _____ with the newly released operating system.",
+      answers: ["compatible", "substantial", "normal", "overseen"],
+      correct: "compatible"
+    },
+    {
+      question: "The introduction of the new system has made a _____ difference in this quarter's sales.",
+      answers: ["valuable", "significant", "preventive", "cautious"],
+      correct: "significant"
+    },
+    {
+      question: "Even though Ms.Palmer is still regarded as a _____ designer, she contributes to many important projects at Graphart Ltd.",
+      answers: ["novice", "critical", "restored", "prohibitive"],
+      correct: "novice"
+    },
+    {
+      question: "The projact manager of the research and development department has an obligation to keep the highly sensitive information.",
+      answers: ["overwhelming", "innovative", "secure", "competitive"],
+      correct: "secure"
+    },
+    {
+      question: "Although a lot research supports Manelli's hypothesis, a _____ conclusion cannot be made until after further studies are performed.",
+      answers: ["convinced", "robust", "flexible", "luxury"],
+      correct: "robust"
+    },
+    {
+      question: "Participation in the upcoming hospitality seminar is strictly _____ but highly recommended.",
+      answers: ["conventional", "challenging", "imaginative", "voluntary"],
+      correct: "voluntary"
+    },
+    {
+      question: "Located near Angler Lake, Hillcrest Hotel has a _____ view that makets it a first choice for accommodations in the region.",
+      answers: ["breathtaking", "professional", "probable", "validated"],
+      correct: "breathtaking"
+    },
+    {
+      question: "Derek added a brief _____ description concerning the benefits package to the employment page of FMX, Inc's Web site.",
+      answers: ["advisable", "considerable", "complimentary", "supplementary"],
+      correct: "supplementary"
+    },
+    {
+      question: "During her presentation, local dietician Janine Harris demonstrated her _____ knowledge of the culinary arts.",
+      answers: ["tentative", "various", "countless", "comprehensive"],
+      correct: "comprehensive"
+    },
+    {
+      question: "Mr.Jhonson was hired to medidate and resolve the _____ negotiations, which have adversely affected company operations.",
+      answers: ["sizable", "nutritional", "pending", "countrary"],
+      correct: "pending"
+    },
+    {
+      question: "The internship that Axion, Inc., offers will be _____ in finding a job on your desired career path.",
+      answers: ["striking", "obsolete", "feasible", "instrumental"],
+      correct: "instrumental"
+    }
+  ];
+
