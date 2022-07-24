0f02e9ef56b66df95674156f409bf5f42e9c1dfe
tjoe22001 <107105433+tjoe22001@users.noreply.github.com>
2022-07-24 18:40:36 +0900

    Add files via upload

---
+++ quiz/js/Adverbs.js
@@ -0,0 +1,89 @@
+//問題文を格納する要素です
+const quiz = [
+    {
+      question: "Nelson Lionet goes abroad on business trips _____ as he works for a foreign trading company.",
+      answers: ["likely", "frequently", "reportedly", "widely"],
+      correct: "frequently"
+    },
+    {
+      question: "After being transferred to the Atlanta branch, Randy Wilkins started producing _____ successful results.",
+      answers: ["selectively", "harshly", "thoughtfully", "immensely"],
+      correct: "immensely"
+    },
+    {
+      question: "The new low-cost carrier Northwest Airways is _____ showing a strong presence in the industry.",
+      answers: ["well", "quite" ,"yet", "already"],
+      correct: "already"
+    },
+    {
+      question: "Due to constraints, Mr.Larson will only _____ explain how to use the app for designing social media campaigns.",
+      answers: ["potentially", "briefly", "indefinitely", "distinctly"],
+      correct: "briefly"
+    },
+    {
+      question: "After compiling the data, Mr.Roswell's team will be _____ analyzing the results to identify correlations.",
+      answers: ["formerly", "agreeably", "fortunately", "thoroughly"],
+      correct: "thoroughly"
+    },
+    {
+      question: "_____ everyone can take part in the workshop as long as they are off duty at the time.",
+      answers: ["Fairly", "Nearly", "Sturdily", "Suitably"],
+      correct: "Nearly"
+    },
+    {
+      question: "All subscribers to Metrin Internet Service Provider _____ receive newsletters from the company.",
+      answers: ["economically", "intensively", "periodically", "concisely"],
+      correct: "periodically"
+    },
+    {
+      question: "A director at the banquet was surprised at how _____ dressed a few of the attendees were.",
+      answers: ["automatically", "shortly", "informally", "persistently"],
+      correct: "informally"
+    },
+    {
+      question: "Many tourists visit San Jose to see the _____ built landmark, the DG Tower Bridge.",
+      answers: ["amply", "undeniably", "currently", "recently"],
+      correct: "recently"
+    },
+    {
+      question: "Since _____ seventy association members are expected to turn out for the luncheon, another table was set up in the dining hall.",
+      answers: ["confidentially", "positively", "intentionally", "approximately"],
+      correct: "approximately"
+    },
+    {
+      question: "The new accounting software has proven to be more useful than _____ thought.",
+      answers: ["initially", "steadily", "specifically", "conveniently"],
+      correct: "initially"
+    },
+    {
+      question: "Moore Auto succeeded in advertising their new model _____ to their loyal customers.",
+      answers: ["relatively", "effectively", "exactly", "officially"],
+      correct: "effectively"
+    },
+    {
+      question: "The CEO announced today in the press conference that the new project is progressing _____ thus far.",
+      answers: ["generously", "entirely", "repeatedly", "satisfactorily"],
+      correct: "satisfactorily"
+    },
+    {
+      question: "Copies of the purchase orders are filed _____ in the cabinet near the copy machine.",
+      answers: ["persuasively", "structurally", "measurably", "numerically"],
+      correct: "numerically"
+    },
+    {
+      question: "ETX's sales figures have dropped _____ over the the last three months since CEO Michael Kirk resigned.",
+      answers: ["efficiently", "sharply", "diversely", "personally"],
+      correct: "sharply"
+    },
+    {
+      question: "All city officials are obliged to adhere _____ to the highest ethical standards.",
+      answers: ["strategically", "previously", "strictly", "delicately"],
+      correct: "strictly"
+    },
+    {
+      question: "The annual branch managers' meeting is scheduled to be held at the head office at _____ 3:00 P.M. next Wednesday.",
+      answers: ["precisely", "exclusively", "extremely", "actually"],
+      correct: "precisely"
+    }
+  ];
+
---
+++ quiz/js/PreposiConju.js
@@ -0,0 +1,44 @@
+//問題文を格納する要素です
+const quiz = [
+    {
+      question: "An important role of middle managers is to mediate ______ labor and the board.",
+      answers: ["between", "despite", "until", "without"],
+      correct: "between"
+    },
+    {
+      question: "_____ visiting many countries, Ms.Murphy learned a lot of managerial strategies.",
+      answers: ["Because", "If", "While", "Though"],
+      correct: "While"
+    },
+    {
+      question: "The market share of Sonite Electric's high-definition television has expanded _____ the globe.",
+      answers: ["across", "opposite" ,"amid", "except"],
+      correct: "across"
+    },
+    {
+      question: "_____ the Los Angeles store is close to the city center, the Seattle location is in a suburban area.",
+      answers: ["Therefore", "However", "Whereas", "Meanwhile"],
+      correct: "Whereas"
+    },
+    {
+      question: "_____ a competitive salary, leading distributor Colex, Inc., offers a variety of benefits to employees.",
+      answers: ["In response to", "In place of", "In addition to", "In anticipation of"],
+      correct: "In addition to"
+    },
+    {
+      question: "Dave Potter visited the venue for the conference _____ to make sure that the preparations were going smoothly.",
+      answers: ["as opposed", "in regard", "in advance", "by means"],
+      correct: "in advance"
+    },
+    {
+      question: "Flight attendants have to help all passengers on board evacuate quickly _____ an emergency.",
+      answers: ["in terms of", "in the event of", "in accordance with", "in compliance with"],
+      correct: "in the event of"
+    },
+    {
+      question: "Kosno's new tablet has become popular _____ young people thanks to targeted marketing on a social networking site.",
+      answers: ["among", "during", "within", "past"],
+      correct: "among"
+    }
+  ];
+
